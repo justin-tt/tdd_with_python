38b29153ebcd6799e14f1a8ea92238a509e886c2
justin <justin.ch.goh@gmail.com>
2018-03-28 00:17:11 +0800

    Refactored functional tests to use a helper function

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,12 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+    
+    # helper methods don't run, as long as you don't prefix them with test
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about an app. She goes online to check out 
@@ -35,10 +41,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in the todo list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         # There is still a textbox waiting for her to enter another item.
         # She enters "Use peacock feathers to make a fly"
@@ -48,11 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         
         # The page updates again, and now she has both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',
-            [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
         # She wonders if the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is 
