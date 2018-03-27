ab5d257005f1105d246994bf34bc55329a1fe841
justin <justin.ch.goh@gmail.com>
2018-03-28 00:02:10 +0800

    before refactoring DRY lines

--- functional_tests.py
+++ functional_tests.py
@@ -38,20 +38,26 @@ class NewVisitorTest(unittest.TestCase):
         
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         
         # There is still a textbox waiting for her to enter another item.
         # She enters "Use peacock feathers to make a fly"
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         
         # The page updates again, and now she has both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',
+            [row.text for row in rows])
         
         # She wonders if the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is 
         # some explanatory note to that effect.
+        self.fail('Finish the test!')
         
         # She visits that URL- her to-do list is still there
         
@@ -59,3 +65,4 @@ class NewVisitorTest(unittest.TestCase):
         
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
+
