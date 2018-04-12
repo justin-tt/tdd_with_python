a2cb73df2c3c932662e918893efee2aab7136a99
justin <justin.ch.goh@gmail.com>
2018-04-12 23:58:15 +0800

    fixed gunicorn service, socket not source

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,16 +2,17 @@ from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 import random
 
-env.domain = 'staging.tlojewellery.com'
+# print(env.hosts)
+# env.domain = 'staging.tlojewellery.com'
 
 REPO_URL = 'https://github.com/justin-tt/tdd_with_python'
 
 def deploy():
-    site_folder = f'/home/{env.user}/sites/{env.domain}'
+    site_folder = f'/home/{env.user}/sites/{env.host}'
     source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
-    _update_settings(source_folder, env.domain)
+    _update_settings(source_folder, env.host)
     _update_virtualenv(source_folder)
     _update_static_files(source_folder)
     _update_database(source_folder)
