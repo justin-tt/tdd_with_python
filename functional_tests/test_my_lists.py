357f8783e63d1595cab46203335ca0eb987ea063
justin <justin.ch.goh@gmail.com>
2018-05-13 22:45:20 +0800

    add list owners via forms. more isolated tests

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -42,7 +42,7 @@ class MyListsTest(FunctionalTest):
         self.wait_for(
             lambda: self.browser.find_element_by_link_text('Reticulate splines')
         )
-        self.browser.find_element_by_link_text('Reticulate splines')
+        self.browser.find_element_by_link_text('Reticulate splines').click()
         self.wait_for(
             lambda: self.assertEqual(self.browser.current_url, first_list_url)
         )
@@ -65,6 +65,6 @@ class MyListsTest(FunctionalTest):
         # She logs out. The "My lists" option disappears
         self.browser.find_element_by_link_text('Log out').click()
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_link_text('My lists'),
+            self.browser.find_elements_by_link_text('My lists'),
             []
         ))
--- lists/views.py
+++ lists/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 from lists.models import Item, List
-from lists.forms import ItemForm, ExistingListItemForm
+from lists.forms import ItemForm, ExistingListItemForm, NewListForm
 
 User = get_user_model()
 
@@ -22,16 +22,13 @@ def view_list(request, list_id):
         'list': list_, 'form': form
     })
 
+
 def new_list(request):
-    form = ItemForm(data=request.POST)
+    form = NewListForm(data=request.POST)
     if form.is_valid():
-        list_ = List()
-        list_.owner = request.user
-        list_.save()
-        form.save(for_list=list_)
+        list_ = form.save(owner=request.user)
         return redirect(list_)
-    else:
-        return render(request, 'home.html', {"form": form})
+    return render(request, 'home.html', {'form': form})
 
 def my_lists(request, email):
     owner = User.objects.get(email=email)
